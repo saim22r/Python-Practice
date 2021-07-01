202419cf3234691fc1a1706f1e7dc3b47720da4a
unknown <saimrafiq224@gmail.com>
2021-07-01 10:35:32 +0100

    Update

--- Python_practice.py
+++ Python_practice.py
@@ -12,3 +12,12 @@
 # for i in list:
 #     print(i)
 
+# Create, AND, &&, &, ==  Which one returns a boolean? ==
+
+# # What is the difference between a list and a tuple?
+# A list [] is mutable whereas a tuple () is immutable
+
+# Can we add an element to a list? Yes
+# Can we add an element to a tuple? No
+# Can the element of a tuple be different types? Yes
+
