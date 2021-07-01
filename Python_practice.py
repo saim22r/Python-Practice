9d82734a2e1f1c0a7310c93d852c4a5b4fdfde63
unknown <saimrafiq224@gmail.com>
2021-07-01 10:24:50 +0100

    Update

--- Python_practice.py
+++ Python_practice.py
@@ -1,6 +1,14 @@
-# Declare a function called username that takes one argument as a string and return the name
+# # Declare a function called username that takes one argument as a string and return the name
+#
+# def username(name):
+#     return name
+#
+# print(username("Saim"))
 
-def username(name):
-    return name
+# # Declare a list with numbers 1 - 5 and iterate through the list and display the list
+#
+# list = [1, 2, 3, 4, 5]
+#
+# for i in list:
+#     print(i)
 
-print(username("Saim"))
