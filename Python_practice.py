f023f7ff6d21bbfae3cfa420fea96406a336c7af
unknown <saimrafiq224@gmail.com>
2021-07-01 10:39:00 +0100

    Update

--- Python_practice.py
+++ Python_practice.py
@@ -1,23 +1,27 @@
-# # Declare a function called username that takes one argument as a string and return the name
+# 1. Declare a function called username that takes one argument as a string and return the name
 #
 # def username(name):
 #     return name
 #
 # print(username("Saim"))
 
-# # Declare a list with numbers 1 - 5 and iterate through the list and display the list
+# 2. Declare a list with numbers 1 - 5 and iterate through the list and display the list
 #
 # list = [1, 2, 3, 4, 5]
 #
 # for i in list:
 #     print(i)
 
-# Create, AND, &&, &, ==  Which one returns a boolean? ==
+# 3. Create, AND, &&, &, ==  Which one returns a boolean? ==
 
-# # What is the difference between a list and a tuple?
+# 4. What is the difference between a list and a tuple?
 # A list [] is mutable whereas a tuple () is immutable
 
-# Can we add an element to a list? Yes
+# 5. Can we add an element to a list? Yes
 # Can we add an element to a tuple? No
 # Can the element of a tuple be different types? Yes
+
+# 6. Create a dictionary with key value pairs firstname and lastname
+# dict = {"firstname": "Saim", "lastname": "Rafiq"}
+
 
